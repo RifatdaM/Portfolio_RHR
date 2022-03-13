2541f420626faf223739ec72e43a1c1a69e96955
Rakibul Hasan <rifat20089@hotmail.com>
2022-03-13 12:07:48 +0600

    All file created and tailwind setup done

---
+++ src/components/contacts/Contacts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../index.css'
+
+const Contacts = () => {
+  return (
+    <div>Contacts</div>
+  )
+}
+
+export default Contacts
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../index.css'
+
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
