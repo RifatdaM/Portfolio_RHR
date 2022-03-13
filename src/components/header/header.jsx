bd785a3b540c6308fcfee5cebc5fe03349925fe0
Rakibul Hasan <rifat20089@hotmail.com>
2022-03-13 22:51:00 +0600

    working on header section

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import '../../index.css'
+import React from "react";
+import "../../index.css";
+import CTA from "./CTA";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-light">Hello I'm</h1>
+        <h1 className="text-3xl font-bold">Rakibul Hasan Rifat</h1>
+        <h5 className="font-xl text-gray-400 font-extralight">UI/UX Designer and Frontend Developer</h5>
+        <CTA/>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
