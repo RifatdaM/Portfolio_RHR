d1d210f6604c4b044710ff74bb32b12cac4d6d73
Rakibul Hasan <rifat20089@hotmail.com>
2022-03-22 12:44:45 +0600

    portfolio section done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,166 @@
-import React from 'react'
-import '../../index.css'
+import React from "react";
+import "../../index.css";
+import IMG1 from "../../assets/project1.jpg";
+import IMG2 from "../../assets/project2.jpg";
+import IMG3 from "../../assets/project3.jpg";
+import IMG4 from "../../assets/project4.png";
+import IMG5 from "../../assets/project5.png";
+import IMG6 from "../../assets/project6.jpg";
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5 className="title-small">My Recent Work</h5>
+      <h2 className="title">Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={IMG1} alt="Book Drop a proper library Managment system" />
+          </div>
+          <h3>Book Drop - Library Managment App</h3>
+          <div className="item-cta">
+            <a
+              href="https://github.com/Fantastic-Four-CSC-370/Book-Drop"
+              className="btn"
+              target="_blank"
+              rel="noopener"
+            >
+              Github
+            </a>
+            <a
+              href=""
+              className="btn btn-primay"
+              target="_blank"
+              rel="noopener"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={IMG1} alt="Book Drop a proper library Managment system" />
+          </div>
+          <h3>Book Drop - Library Managment App</h3>
+          <div className="item-cta">
+            <a
+              href="https://github.com/Fantastic-Four-CSC-370/Book-Drop"
+              className="btn"
+              target="_blank"
+              rel="noopener"
+            >
+              Github
+            </a>
+            <a
+              href=""
+              className="btn btn-primay"
+              target="_blank"
+              rel="noopener"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={IMG1} alt="Book Drop a proper library Managment system" />
+          </div>
+          <h3>Book Drop - Library Managment App</h3>
+          <div className="item-cta">
+            <a
+              href="https://github.com/Fantastic-Four-CSC-370/Book-Drop"
+              className="btn"
+              target="_blank"
+              rel="noopener"
+            >
+              Github
+            </a>
+            <a
+              href=""
+              className="btn btn-primay"
+              target="_blank"
+              rel="noopener"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={IMG1} alt="Book Drop a proper library Managment system" />
+          </div>
+          <h3>Book Drop - Library Managment App</h3>
+          <div className="item-cta">
+            <a
+              href="https://github.com/Fantastic-Four-CSC-370/Book-Drop"
+              className="btn"
+              target="_blank"
+              rel="noopener"
+            >
+              Github
+            </a>
+            <a
+              href=""
+              className="btn btn-primay"
+              target="_blank"
+              rel="noopener"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={IMG1} alt="Book Drop a proper library Managment system" />
+          </div>
+          <h3>Book Drop - Library Managment App</h3>
+          <div className="item-cta">
+            <a
+              href="https://github.com/Fantastic-Four-CSC-370/Book-Drop"
+              className="btn"
+              target="_blank"
+              rel="noopener"
+            >
+              Github
+            </a>
+            <a
+              href=""
+              className="btn btn-primay"
+              target="_blank"
+              rel="noopener"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={IMG1} alt="Book Drop a proper library Managment system" />
+          </div>
+          <h3>Book Drop - Library Managment App</h3>
+          <div className="item-cta">
+            <a
+              href="https://github.com/Fantastic-Four-CSC-370/Book-Drop"
+              className="btn"
+              target="_blank"
+              rel="noopener"
+            >
+              Github
+            </a>
+            <a
+              href=""
+              className="btn btn-primay"
+              target="_blank"
+              rel="noopener"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
