87580372d7a63dbec60439e1b6804d2b40921c71
Rakibul Hasan <rifat20089@hotmail.com>
2022-03-28 12:56:56 +0600

    testimonial done

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import '../../index.css'
+import React from "react";
+import "../../index.css";
 
 const Contacts = () => {
   return (
-    <section id='contact'>Contacts</section>
-  )
-}
+    <section id="contact">
+      <h5 className="title-small">Keep in Touch</h5>
+      <h2 className="title">Contact</h2>
+    </section>
+  );
+};
 
-export default Contacts
+export default Contacts;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import '../../index.css'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "../../index.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Hasibur Rahman Shuvo",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt culpa veniam soluta rerum voluptates ipsum temporibus animi ad eaque hic, dolorem consequuntur eum quasi, quidem atque ratione voluptatem cupiditate placeat?",
+  },
+  {
+    avatar: AVTR2,
+    name: "Shaan E Alam",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt culpa veniam soluta rerum voluptates ipsum temporibus animi ad eaque hic, dolorem consequuntur eum quasi, quidem atque ratione voluptatem cupiditate placeat?",
+  },
+  {
+    avatar: AVTR3,
+    name: "Sahara Alam",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt culpa veniam soluta rerum voluptates ipsum temporibus animi ad eaque hic, dolorem consequuntur eum quasi, quidem atque ratione voluptatem cupiditate placeat?",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
-  )
-}
+    <section id="testimonials">
+      <h5 className="title-small">Review from clients</h5>
+      <h2 className="title">Testimonials</h2>
 
-export default Testimonials
+      <Swiper
+        spaceBetween={40}
+        slidesPerView={1}
+        Autoplay
+        className="container w-[9%] md:w-[60%] lg:w-[40%] pb-16 mySwiper"
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
