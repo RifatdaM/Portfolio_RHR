fab311298013650ff38d5b388392c256fe990a2c
RiaftdaM <rifat20089@hotmail.com>
2022-08-15 10:51:42 +0600

    Update CV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/RakibulHasabCV.pdf'
 
 const CTA = () => {
   return (
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsBehance} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/" target='_blank'><BsGithub/></a>
-        <a href="https://behance.com/" target='_blank'><BsBehance/></a>
+        <a href="https://www.linkedin.com/in/rifatdam/" target='_blank'><BsLinkedin/></a>
+        <a href="https://github.com/rifatdam" target='_blank'><BsGithub/></a>
+        <a href="https://behance.com/rifatdam" target='_blank'><BsBehance/></a>
     </div>
   )
 }
